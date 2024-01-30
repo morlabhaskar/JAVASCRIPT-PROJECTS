4dcf6bc7d687f3216b134b9d831f61c9bdcb037b
morlabhaskar <morlabhaskar306@gmail.com>
2024-01-30 23:09:47 +0530

    first-commit

---
+++ card-slideing/script.js
@@ -0,0 +1,9 @@
+var slider = document.querySelectorAll('.slider')
+        var uparrow = document.querySelectorAll('.up-arrow')
+        var downarrow = document.querySelector('.down-arrow')
+
+        let x = 0;
+        uparrow.onclick = function() {
+            x = x - 300;
+            slider.style.top = x + "px";
+        }
